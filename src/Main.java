66c110d7fb107891caa7c34029df1c47197c9e06
Marius <m.gzegozevskis@gmail.com>
2019-07-10 23:51:22 +0300

    pirmas commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,36 @@
+class Student{
+    int age;
+    String name;
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+            "age=" + age +
+            ", name='" + name + '\'' +
+            '}';
+    }
+}
 public class Main {
 
     public static void main(String[] args) {
